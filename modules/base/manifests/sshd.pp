a5acedf62c3085f7abd268d0597491c5bb0a4820
Davi <davi@plutao.(none)>
2012-09-11 12:54:03 -0300

    Adding SSH config

---
+++ modules/base/manifests/sshd.pp
@@ -0,0 +1,23 @@
+
+class base::sshd {
+
+  package { 'openssh-server':
+    ensure => installed,
+  }
+  
+  file { '/etc/ssh/sshd_config':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '600',
+    require => Package['openssh-server'],
+  }
+  
+  service { 'ssh':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => File['/etc/ssh/sshd_config']
+  }
+
+}
