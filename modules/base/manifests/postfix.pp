d5d9f23e7e65b3c0308df672c31799d2fbe628f9
Davi Oliveira <arlocm@gmail.com>
2012-09-11 15:18:03 -0300

    Adding postfix config

---
+++ modules/base/manifests/postfix.pp
@@ -0,0 +1,15 @@
+
+class base::postfix {
+
+  package { 'postfix': }
+  package { 'mailutils': }
+
+  service { 'postfix':
+    ensure => running,
+    enable => true,
+    hasstatus => false,
+    stop => 'invoke-rc.d postfix stop',
+    start => 'invoke-rc.d postfix start',
+    restart => 'invoke-rc.d postfix restart',
+  }
+}
