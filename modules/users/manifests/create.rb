010eb89e2f55e9f388d5bd070b20792ead51793c
Davi Campos <arlocm@gmail.com>
2012-10-18 10:30:42 -0300

    Adding user create with authorized_keys

--- modules/users/manifests/create.rb
+++ modules/users/manifests/create.rb
@@ -1,18 +1,44 @@
+# Data Template:
+# users:
+#   user1: 
+#     ssh_authorized_keys: 
+#       - key1
+#       - key2
+#     groups:
+#       - group1
+#       - group2
+#       - group3
+#     ensure: present
+#     other_users_config...
+#    user2:
+#    ...
+
 define "users::create", :users do
 
-  group "sudo", :ensure => :present
+  @users.collect do |username,user_data|
 
-  @users.collect do |username,sshkey|
-    user username, 
-      :ensure => :present, 
-      :managehome => :true, 
-      :groups => "sudo",
-      :require => "Group[sudo]"
+    authorized_keys = user_data.delete('ssh_authorized_keys')
 
-    ssh_authorized_key username,
-      :user => username,
-      :type => 'rsa',
-      :key => sshkey,
-      :require => "User[" << username << "]"
+    user username, user_data
+
+    unless authorized_keys.nil?
+
+      ssh_authorized_key_params = {
+          :user => username,
+          :type => 'rsa',
+          :require => "User[" << username << "]"
+      }
+      if authorized_keys.kind_of?(String)
+        res_name = username + "_" + authorized_keys
+        ssh_authorized_key_params[:key] = authorized_keys
+        ssh_authorized_key res_name, ssh_authorized_key_params
+      else
+        authorized_keys.each do |authorized_key|
+          res_name = username + "_" + authorized_key
+          ssh_authorized_key_params[:key] = authorized_key
+          ssh_authorized_key res_name, ssh_authorized_key_params
+        end
+      end
+    end
   end
 end
--- modules/users/tests/init.pp
+++ modules/users/tests/init.pp
@@ -1,7 +1,7 @@
 
-users::create { "fake_users": 
+users::create { "fake_users1": 
   users => {
-    "fake_user1" => "fakekey1",
-    "fake_user2" => "fakekey2"
+    "fake_user1" => {"ssh_authorized_keys" => "fake_key"},
+    "fake_user2" => {"ssh_authorized_keys" => ["fake_key", "fake_key2"]}
   }
 }
