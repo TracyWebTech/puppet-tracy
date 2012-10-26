a4ac9faafa69e54005844b46ef2b4ed1c9d42d11
Davi Campos <arlocm@gmail.com>
2012-10-26 14:44:10 -0200

    Moving users module to TracyWebTech/puppet-users

--- modules/users/tests/init.pp
+++
@@ -1,7 +0,0 @@
-
-users::create { "fake_users1": 
-  users => {
-    "fake_user1" => {"ssh_authorized_keys" => "fake_key"},
-    "fake_user2" => {"ssh_authorized_keys" => ["fake_key", "fake_key2"]}
-  }
-}
